5f1a5ee542169ac3f11d9674e2dc27567f9db9c5
testandr <testandr31@gmail.com>
2021-09-24 14:05:25 +0300

    initial commit

---
+++ src/main/java/test.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test {
+    public static void main(String[] args) {
+        System.setProperty(
+                "webdriver.chrome.driver", "src/main/resources/chromedriver.exe"
+        );
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+    }
+}
